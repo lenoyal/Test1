cb1031f2f7abcefbe7926f14781dbb6f12329976
lenoyal <jgonzalez@xpanxion.com>
2022-07-24 10:59:22 -0400

    first commit

---
+++ spring-boot-starter-data-jpa/spring-boot-starter-data-jpa/src/main/java/com/springboot/datajpa/SpringBootStarterDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterDataJpaApplication.class, args);
+	}
+
+}
